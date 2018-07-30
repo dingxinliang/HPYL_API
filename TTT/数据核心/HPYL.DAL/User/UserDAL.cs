f3a1bae47f63616977c4256233ac6ef2cd75157f
Yzl <Yzl727@163.com>
2018-07-30 09:52:11 +0800

    测试

---
+++ TTT/数据核心/HPYL.DAL/User/UserDAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Maticsoft.DBUtility;
+using MySql.Data.MySqlClient;
+
+namespace HPYL.DAL.User
+{
+    public class UserDAL
+    {
+        /// <summary>
+        /// 判断用户是否存在
+        /// </summary>
+        /// <param name="UserName"></param>
+        /// <returns></returns>
+        public bool ExistsRegisterPhone(string UserName)
+        {
+            string strSql = "select UserName from himall_members where UserName='"+ UserName + "'";
+            int rows = DbHelperMySQL.ExecuteSql(strSql);
+            if (rows > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ TTT/数据模型/HPYL.Model/BaseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPYL.Model
+{
+    public class BaseModel
+    {
+        //签名
+        public string Sign { get; set; }
+        //Token
+        //public string Token { get; set; }
+        //时间戳
+        public string Ts { get; set; }
+    }
+}
